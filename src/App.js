fa0512590c389e5c838965624bfe88b19bf958ec
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2023-07-06 21:49:14 +0700

    state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
-
-const infor = {
-  name: "Minh Hieu",
-  age: "20",
-};
-
-var Member = (props) => {
-  return (
-    <div className="member">
-      <h1>{props.infor.name}</h1>
-      <h2>{props.infor.age}</h2>
-    </div>
-  );
-};
+import React from "react";
+class Member extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Minh Hieu",
+      age: "20",
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.name}</h1>
+        <h2>{this.state.age}</h2>
+      </div>
+    );
+  }
+}
 
 function App() {
-  return (
-    <div>
-      <Member infor={infor} />
-    </div>
-  );
+  return <div>
+    <Member/>
+  </div>;
 }
 
 export default App;
