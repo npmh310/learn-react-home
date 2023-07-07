fcace24aff68cce8b28386042ad5af2f99bf2267
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2023-07-07 15:27:41 +0700

    form

--- src/App.js
+++ src/App.js
@@ -2,47 +2,41 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 
-class Member extends React.Component {
+class InputForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      user: [
-        { name: "Minh Hieu", age: "20" },
-        { name: "Duong Minh", age: "30" },
-        { name: "Lam Nghia", age: "32" },
-      ],
-      job: "Coder",
+    this.state ={
+      value: "",
+      submit: "",
     };
   }
-  changeName = () => {
-    this.setState ({
-      user: [
-        { name: "Minh Ngoc", age: "22" },
-        { name: "Thi Nhu", age: "31" },
-        { name: "Huu Lam", age: "22" },
-      ],
-      job: "Tester",
+
+  handleValue = (event) => {
+    this.setState({
+      value: event.target.value,
+    });
+  };
+  handleSubmit = () => {
+    this.setState({
+      submit: this.state.value,
     });
   };
 
-  render() {
-    return (
+  render(){
+    return(
       <div>
-        <h4>{this.state.user[0].name}</h4>
-        <h4>{this.state.user[0].age}</h4>
-        <h3>{this.state.job}</h3>
-        <button type="button" onClick={this.changeName}>
-          Change
-        </button>
+        <input type="text" value={this.state.value}  onChange={this.handleValue}/>
+        <button type="button" onClick={this.handleSubmit} > Change </button>
+        <h3>{this.state.submit}</h3>
       </div>
-    );
+    )
   }
 }
 
 function App() {
   return (
     <div>
-      <Member />
+      <InputForm />
     </div>
   );
 }
