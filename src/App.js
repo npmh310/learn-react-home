fdac6e608be305951d0d2b086afaba2a6a7b1795
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2023-07-07 10:25:51 +0700

    xu ly su kien event

--- src/App.js
+++ src/App.js
@@ -6,23 +6,31 @@ class Member extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Minh Hieu",
-      age: "20",
+      user: [
+        { name: "Minh Hieu", age: "20" },
+        { name: "Duong Minh", age: "30" },
+        { name: "Lam Nghia", age: "32" },
+      ],
+      job: "Coder",
     };
   }
-
   changeName = () => {
-    this.setState({
-      name: "Hieu 2",
-      age: "20",
+    this.setState ({
+      user: [
+        { name: "Minh Ngoc", age: "22" },
+        { name: "Thi Nhu", age: "31" },
+        { name: "Huu Lam", age: "22" },
+      ],
+      job: "Tester",
     });
   };
 
   render() {
     return (
       <div>
-        <h4>{this.state.name}</h4>
-        <h4>{this.state.age}</h4>
+        <h4>{this.state.user[0].name}</h4>
+        <h4>{this.state.user[0].age}</h4>
+        <h3>{this.state.job}</h3>
         <button type="button" onClick={this.changeName}>
           Change
         </button>
