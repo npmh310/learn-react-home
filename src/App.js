4b9da5b9a1b442700545804a9fcb2761b45d4e51
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2023-07-07 08:17:48 +0700

    set states easy

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+
 class Member extends React.Component {
-  state = {
-    name: "Minh Hieu",
-    age: "20",
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Minh Hieu",
+      age: "20",
+    };
+  }
+
+  changeName = () => {
+    this.setState({
+      name: "Hieu 2",
+      age: "20",
+    });
   };
 
   render() {
     return (
       <div>
-        <h1>{this.state.name}</h1>
-        <h2>{this.state.age}</h2>
+        <h4>{this.state.name}</h4>
+        <h4>{this.state.age}</h4>
+        <button type="button" onClick={this.changeName}>
+          Change
+        </button>
       </div>
     );
   }
