04bc21e62db1c231624b51bef65099ccabec0323
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2023-07-09 14:41:04 +0700

    user dishes nâng cao tách thành 1 folder riêng

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -1,59 +1,71 @@
 import React from "react";
 import { Media } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
 
 class Menu extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: [
-        {
-          id: 0,
-          name: "Avatar meo",
-          img: "../assets/images/5d2e757e470bbbc1894262ab789a4181.jpg",
-          description: 'đây là con mèo xinh nhất vụ trụ hehehehe'
-        },
-        {
-          id: 1,
-          name: "Avatar member",
-          img: "../assets/images/avatar2.png",
-          description: 'đây là avatar con ngươi'
-        },
-        {
-          id: 2,
-          name: "Avatar cho",
-          img: "../assets/images/cho-Shiba-hoat-hinh-1.jpg",
-          description: 'đây là con chó xinh nhất vụ trụ hehehehe'
-        },
-      ],
+      selectedDish: null,
     };
   }
 
-  render() {
-    const menu = this.state.dishes.map((ownDish) => {
+  onDishSelect(dish) {
+    this.setState({
+      selectedDish: dish,
+    });
+  }
+  renderDish(dish) {
+    if (dish != null)
       return (
-        <div key={ownDish.id} className="col-12 mt-5">
-          <Media tag="li">
-            <Media left middle>
-              <Media
-                object
-                style={{ width: "100px", height: "100px" }}
-                src={ownDish.img}
-                alt={ownDish.name}
-              />
-            </Media>
-            <Media body className="ml-5">
-              <Media heading>{ownDish.name}</Media>
-              <p>{ownDish.description}</p>
-            </Media>
-          </Media>
+        //render ra thằng vừa click vào
+        <div className="col-md-6">
+          <Card>
+            <CardImg top src={dish.img} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
         </div>
+      );
+    else
+      return (
+        <div
+          style={{ height: "100px", width: "100px", border: "1px solid black" }}
+        >
+          chưa có gì hết
+        </div>
+      );
+  }
+
+  render() {
+    //   Render ra từng thằng ở trên
+    const menu = this.props.dishes.map((ownDish) => {
+      return (
+        <div className="col-12 col-md-2 m-1">
+          <Card key={ownDish.id} onClick={() => this.onDishSelect(ownDish)}>
+            <CardImg width="100%" src={ownDish.img} alt={ownDish.name} />
+            <CardBody>
+              <CardTitle>{ownDish.name}</CardTitle>
+            </CardBody>
+          </Card>
+        </div>
+        // </div>
       );
     });
     return (
       <div className="container">
-        <div className="row">
-          {/* <Media list>{menu}</Media> */}
-          {menu}
+        <div className="row  d-flex justify-content-center">{menu}</div>
+        <div className="row mt-5 d-flex justify-content-center">
+          {this.renderDish(this.state.selectedDish)}
         </div>
       </div>
     );
---
+++ src/shared/dishes.js
@@ -0,0 +1,34 @@
+export const DISHES = [
+  {
+    id: 0,
+    name: "Con meo 1",
+    img: "../assets/images/hinh-meo-cute-chibi-5.jpg",
+    category: "mains",
+    description:
+      "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+  },
+  {
+    id: 1,
+    name: "Con cho",
+    img: "../assets/images/cho-Shiba-hoat-hinh-1.jpg",
+    category: "appetizer",
+description:
+      "Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce",
+  },
+  {
+    id: 2,
+    name: "Facebook",
+    img: "../assets/images/avatar2.png",
+    category: "appetizer",
+  description:
+      "A quintessential ConFusion experience, is it a vada or is it a donut?",
+  },
+  {
+    id: 3,
+    name: "Con meo 2",
+    img: "assets/images/5d2e757e470bbbc1894262ab789a4181.jpg",
+    category: "dessert",
+    description:
+      "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
+  },
+];
