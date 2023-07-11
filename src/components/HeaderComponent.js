b281a90eab14967d863a24c6198da12037610120
minhhieu310 <nguyenphuocminhhieu310@gmail.com>
2023-07-11 16:05:42 +0700

    uncontroller login form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -8,60 +8,135 @@ import {
   NavItem,
   InputGroup,
   Button,
-  NavbarToggler
+  NavbarToggler,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Label,
 } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
 class Header extends React.Component {
   constructor(props) {
-      super(props);
+    super(props);
+    this.state = {
+      isNavOpen: false,
+      isModalOpen: false,
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
   }
 
-  render() {
-      return(
-          <div>
-              <div>
-        <Navbar className="header" light expand="md">
-          <NavbarBrand className="col-md-3" href="/">
-            <img
-              src="/assets/images/toppng.com-instagram-logo-transparent-png-512x512.png"
-              alt="Logo"
-              className="logo"
-            />
-          </NavbarBrand>
-          <Nav className="col-md-6 mr-auto" navbar>
-            <NavItem>
-              <NavLink className="nav-link" to="/home">
-                Home
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="nav-link" to="/menu">
-                Menu
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="nav-link" to="/contact">
-                Contact
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="nav-link" to="/aboutUs">
-                About us
-              </NavLink>
-            </NavItem>
-          </Nav>
-          <InputGroup className="col-md-3 search-bar">
-            <Input placeholder="Search..." />
-            <Button color="secondary">Search</Button>
-          </InputGroup>
-        </Navbar>
-      </div>
-             
-          </div>
-      );
-  }
+  handleLogin(event) {
+    this.toggleModal();
+    alert("Username: " + this.username.value + " Password: " + this.password.value
+        + " Remember: " + this.remember.checked);
+    event.preventDefault();
+
 }
 
+  render() {
+    return (
+      <div>
+        <div>
+          <Navbar className="header" light expand="md">
+            <NavbarBrand className="col-md-3" href="/">
+              <img
+                src="/assets/images/toppng.com-instagram-logo-transparent-png-512x512.png"
+                alt="Logo"
+                className="logo"
+              />
+            </NavbarBrand>
+            <Nav className="col-md-6 mr-auto" navbar>
+              <NavItem>
+                <NavLink className="nav-link" to="/home">
+                  Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/menu">
+                  Menu
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/contact">
+                  Contact
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="nav-link" to="/aboutUs">
+                  About us
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <InputGroup className="col-md-3 search-bar">
+              <Input placeholder="Search..." />
+              <Button color="secondary">Search</Button>
+            </InputGroup>
+          </Navbar>
+        </div>
+
+        {/* // form login */}
+        <div className="container mt-4">
+     
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <Button outline onClick={this.toggleModal}>
+                <span className="fa fa-sign-in fa-lg"></span> Login
+              </Button>
+            </NavItem>
+          </Nav>
+          <Modal isOpen={this.state.isModalOpen}  toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+            <ModalBody>
+              {/* form login trong the form*/}
+              <Form onSubmit={this.handleLogin}>
+                <FormGroup>
+                  <Label htmlFor="username">Username</Label>
+                  <Input
+                    type="text"
+                    id="username"
+                    name="username"
+                    innerRef={(input) => (this.username = input)}
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    type="password"
+                    id="password"
+                    name="password"
+                    innerRef={(input) => (this.password = input)}
+                  />
+                </FormGroup>
+                <FormGroup check>
+                  <Label check>
+                    <Input
+                      type="checkbox"
+                      name="remember"
+                      innerRef={(input) => (this.remember = input)}
+                    />
+                    Remember me
+                  </Label>
+                </FormGroup>
+                <Button type="submit" value="submit" color="primary">
+                  Login
+                </Button>
+              </Form>
+            </ModalBody>
+          </Modal>
+        </div>
+        
+      </div>
+    );
+  }
+}
 
 export default Header;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,63 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
+//uncontrolled
 function Login() {
+  const formRef = useRef(null);
   const [name, setName] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(`Name: ${name}`);
-    alert(`Name: ${name}`)
+    const formDate = new FormData(formRef.current);
+    console.log(`Name: ${formDate.get("name")}`);
+    alert(`Name: ${formDate.get("name")}`);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form ref={formRef} onSubmit={handleSubmit}>
       <label>
         Name:
         <input
           type="text"
           value={name}
+          placeholder="Please input your name"
+          name="name"
           onChange={(event) => {
             setName(event.target.value);
           }}
         ></input>
         <br></br>
       </label>
-      <button type='submit' className="btn btn-success">submit</button>
+      <button type="submit" className="btn btn-success">
+        submit
+      </button>
     </form>
   );
 }
+
+//have controlled
+// function Login() {
+//   const [name, setName] = useState("");
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     console.log(`Name: ${name}`);
+//     alert(`Name: ${name}`)
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <label>
+//         Name:
+//         <input
+//           type="text"
+//           value={name}
+//           onChange={(event) => {
+//             setName(event.target.value);
+//           }}
+//         ></input>
+//         <br></br>
+//       </label>
+//       <button type='submit' className="btn btn-success">submit</button>
+//     </form>
+//   );
+// }
 
 export default Login;
